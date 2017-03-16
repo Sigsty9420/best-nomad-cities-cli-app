866693c4a3648625a4816313587c41b4d4810d8f
bortch89 <bortch@gmail.com>
2017-03-16 12:24:00 +0200

    changed #search_city to also be searchable bu number and made choices more userfriendly + looping back to start or exiting if necessary.

--- lib/best_nomad_cities/cli.rb
+++ lib/best_nomad_cities/cli.rb
@@ -26,11 +26,24 @@ class BestNomadCities::CLI
       puts "Please enter city name. If located in the US, please add the state if possible. (Example: 'New York, NY')"
       puts ''
       search = gets.strip
-      search_city
+      search_city(search)
+      puts ''
+      puts "Would you like to see other cities? (Y or N)"
+      y_or_n = gets.strip
+      if y_or_n == "Y"
+        start
+      else
+        puts ''
+        puts "'Don't listen to what they say. GO SEE!'"
+        exit
     elsif choice == 3
       puts ''
       puts "'A good traveller has no fixed plans and is not intent on arriving.' - Lao Tzu"
       exit
+    else
+      puts ''
+      puts "If at first you don't succeed, try again."
+      start
     end
   end
 
@@ -41,5 +54,26 @@ class BestNomadCities::CLI
     BestNomadCities::City.all[0, amount].each.with_index(1) do |city, index|
       puts "#{index}. #{city.name}, #{city.country}"
     end
+  end
+
+  def search_city_by_name_or_number(search)
+    if search.length > 3
+      found_city = BestNomadCities::City.all.select {|city| city.name.start_with?(search)}
+    else
+      found_city = BestNomadCities::City.all[search.to_i]
+    end
+    puts ''
+    puts "---------- City: #{found_city.name} ----------"
+    puts ''
+    puts "Global nomad rank:            #{found_city.rank}"
+    puts "Country:                #{found_city.country}"
+    puts "Nomad cost:             #{found_city.cost}"
+    puts "Internet speed:         #{found_city.internet_speed}"
+    puts "Weather (now):          #{found_city.weather}"
+    puts "Link to city page:      #{found_city.link}"
+    puts "Link to travel guide:   #{found_city.tg_link}"
+    puts ''
+    puts "Hold 'cmd' on Mac to click the links."
+
   end
 end
